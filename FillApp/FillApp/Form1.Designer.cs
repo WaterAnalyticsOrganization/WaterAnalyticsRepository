4d2d8a6ca485d8b343f2f94e24c758417741aadf
Priya-ANZ <priyanayar87@gmail.com>
2014-11-11 20:56:16 +0530

    Added app for generating sensor details
    Added app for generating sensor details

---
+++ FillApp/FillApp/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace FillApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_Generate = new System.Windows.Forms.Button();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(32, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Date:";
+            // 
+            // btn_Generate
+            // 
+            this.btn_Generate.Location = new System.Drawing.Point(84, 95);
+            this.btn_Generate.Name = "btn_Generate";
+            this.btn_Generate.Size = new System.Drawing.Size(75, 23);
+            this.btn_Generate.TabIndex = 2;
+            this.btn_Generate.Text = "Generate";
+            this.btn_Generate.UseVisualStyleBackColor = true;
+            this.btn_Generate.Click += new System.EventHandler(this.btn_Generate_Click);
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(71, 56);
+            this.maskedTextBox1.Mask = "####-##-##";
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.btn_Generate);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_Generate;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+    }
+}
+
---
+++ FillApp/FillApp/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FillApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Generate_Click(object sender, EventArgs e)
+        {
+          int i;
+            string UserID;
+            Random randno = new Random();
+            int quant;
+            string quantity;
+            string date;
+            string final = "";
+            //FileStream fs = File.Open("C:/Hackathon", FileMode.Open, FileAccess.Write, FileShare.None);
+            
+            for (i = 1; i <= 120; i++)
+            {
+                UserID = i.ToString().PadLeft(7,'0');
+              
+              for (int j = 0; j < 2400; j=j+100)
+              {
+                  date = maskedTextBox1.Text.Remove(4,1).Remove(6,1) + j.ToString().PadLeft(4, '0');
+                  quant = randno.Next(100000000, 100000500);
+                  quantity = quant.ToString().PadLeft(11, '0');
+
+                  if (i == 120 && j == 2300)
+                  {
+                      final = final + UserID + date + quantity ;
+                  }
+                  else
+                  {
+                      final = final + UserID + date + quantity + ',';
+                  }
+                           
+              }
+                //Byte[] info = new UTF8Encoding(true).GetBytes(final);
+            
+               // fs.Write(info,0,info.Length);
+
+            }
+            System.IO.File.WriteAllText(@"C:\Hackathon\sensor_Raw"+maskedTextBox1.Text.Remove(4,1).Remove(6,1)+".txt", final);
+
+        }
+   
+      }
+}
